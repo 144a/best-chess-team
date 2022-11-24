03600040a95fda3db615948ba66cf222f70b8837
Dans-class-foplder <117103607+Dans-class-foplder@users.noreply.github.com>
2022-11-23 20:03:42 -0600

    removing GUI instance

--- Chess/src/Engine/chessTester.java
+++ Chess/src/Engine/chessTester.java
@@ -6,12 +6,8 @@ public class chessTester  {
 
 	public static void main(String[] args) {
 		chessGame game = new chessGame("Human", "Human");
-		//GUI g = new GUI();
-		//g.chessGame();
-		new Panel_manager();
-
-
 		game.runGame();
+
 
 
 	}
