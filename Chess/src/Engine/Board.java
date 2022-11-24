98ed11ef0e32302987391392b71a3e627991fc3e
Dans-class-foplder <117103607+Dans-class-foplder@users.noreply.github.com>
2022-11-23 18:41:20 -0600

    adding "getBoard" method and the variable "arr"

--- Chess/src/Engine/Board.java
+++ Chess/src/Engine/Board.java
@@ -1,12 +1,16 @@
 package Engine;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
 	// Fields
 	private Square[][] squares;
 	private Player player1;
 	private Player player2;
+
+	static 	String  board;
+	static String[][] arr ;
 	
 	
 	// Constructor
@@ -33,225 +37,17 @@ public class Board {
 	
 	// Methods
 	public boolean checkIfCheck(Color c) {
-		ArrayList<Piece> p2Pieces = player2.getPieces();
-		ArrayList<Piece> p1Pieces = player1.getPieces();
-		if(c.equals(Color.WHITE)) {
-			Piece king = p1Pieces.get(4);
-			for(Piece p : p2Pieces) {
-				if(p.validate(king.getLocation())) {
-					return true;
-				}
-			}
-		}
-		if(c.equals(Color.BLACK)) {
-			Piece king = p2Pieces.get(4);
-			for(Piece p : p1Pieces) {
-				if(p.validate(king.getLocation())) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	 public boolean checkIfCheckmateKing(Color c) {
-		ArrayList<Piece> otherPieces = new ArrayList<>();
-		ArrayList<Piece> thisPieces = new ArrayList<>();
-		if(c.equals(Color.WHITE)) {
-			otherPieces = player2.getPieces();
-			 thisPieces = player1.getPieces();
-		}
-		else {
-			 thisPieces = player2.getPieces();
-			 otherPieces = player1.getPieces();
-		}
-			Piece king = thisPieces.get(4);
-			Square loc = king.getLocation();
-			ArrayList<Square> potentialMoves = new ArrayList<>();
-			//1. row, col-1
-			if(loc.getLocation()[1] - 1 >= 0) {
-				Square temp = this.getSquare(loc.getLocation()[0], loc.getLocation()[1] - 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			}
-			//2. row, col + 1
-			if(loc.getLocation()[1] + 1 < 8) {
-				Square temp = this.getSquare(loc.getLocation()[0], loc.getLocation()[1] + 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//3. row - 1, col
-			if(loc.getLocation()[0] - 1 >= 0) {
-				Square temp = this.getSquare(loc.getLocation()[0] - 1, loc.getLocation()[1]);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//4. row + 1, col
-			if(loc.getLocation()[0] + 1 < 8) {
-				Square temp = this.getSquare(loc.getLocation()[0] + 1, loc.getLocation()[1]);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//5. row -1, col -1
-			if(loc.getLocation()[0] - 1 >= 0 && loc.getLocation()[1] - 1 >= 0) {
-				Square temp = this.getSquare(loc.getLocation()[0] - 1, loc.getLocation()[1] - 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//6. row -1, col + 1
-			if(loc.getLocation()[0] - 1 >= 0 && loc.getLocation()[1] + 1 < 8) {
-				Square temp = this.getSquare(loc.getLocation()[0] - 1, loc.getLocation()[1] + 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//7. row + 1, col - 1
-			if(loc.getLocation()[0] + 1 < 8 && loc.getLocation()[1] -1 >= 0) {
-				Square temp = this.getSquare(loc.getLocation()[0] + 1, loc.getLocation()[1] - 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//8. row + 1, col + 1
-			if(loc.getLocation()[0] + 1 < 8 && loc.getLocation()[1] + 1 < 8) {
-				Square temp = this.getSquare(loc.getLocation()[0] + 1, loc.getLocation()[1] + 1);
-				if(king.validate(temp)) {
-					if(temp.getPiece() == null) {
-					potentialMoves.add(temp);
-					}
-					else if (!temp.getPiece().getColor().equals(c)) {
-						potentialMoves.add(temp);
-					}
-				}
-			
-			}
-			//check if potential moves are in check
-			int count = 0;
-			for(Square s : potentialMoves) {
-				for(Piece p : otherPieces ) {
-					if(p.validate(s)) {
-						count++;
-						break;
-					}
-				}
-			}
-			if (count >= potentialMoves.size()) {
-				return true;
-			}
-			
-			
-			
+		boolean ret = false;
 		
-		return false;
+		return ret;
 	}
 	
 	public boolean checkIfCheckmate(Color c) {
+		boolean ret = false;
 		
-		ArrayList<Piece> otherPieces = new ArrayList<>();
-		ArrayList<Piece> thisPieces = new ArrayList<>();
-		boolean check = true;
-		if(c.equals(Color.WHITE)) {
-			otherPieces = player2.getPieces();
-			 thisPieces = player1.getPieces();
-		}
-		else {
-			 thisPieces = player2.getPieces();
-			 otherPieces = player1.getPieces();
-		}
-		for(Piece p : thisPieces) {
-			for(int i = 0; i < 8; i++) {
-				if(p.getType() != PieceType.KING) {
-					for(Square s : this.squares[i]) {
-						if(p.validate(s)) {
-							if(s.getPiece() == null) {
-								Square curr = p.getLocation();
-								curr.setPiece(null);
-								s.setPiece(p);
-								p.setLocation(s);
-								check = checkIfCheck(c);
-								curr.setPiece(p);
-								s.setPiece(null);
-								p.setLocation(curr);
-								if(check == false) {
-									return check;
-								}
-							}
-							else if(s.getPiece().getColor() != c) {
-								Square curr = p.getLocation();
-								curr.setPiece(null);
-								Piece capture = s.getPiece();
-								s.setPiece(p);
-								p.setLocation(s);
-								capture.setLocation(null);
-								check = checkIfCheck(c);
-								curr.setPiece(p);
-								s.setPiece(capture);
-								capture.setLocation(s);
-								p.setLocation(curr);
-								if(check == false) {
-									return check;
-									}
-							}
-						}
-					}
-				}
-				else {
-					check = checkIfCheckmateKing(c);
-					if(check == false) {
-						return check;
-					}
-				}
-			}
-		}
-		return true;
-		
+		return ret;
 	}
+	
 	public Square getSquare(int ir, int ic) {
 		return this.squares[ir][ic];
 	}
@@ -286,5 +82,67 @@ public class Board {
 			
 		}
 		System.out.println(" b |A-|B-|C-|D-|E-|F-|G-|H-| b");
+	}
+	public void getBoard() {
+		int counter = 9;
+		String board = "";
+
+		board +=("TATBTCTDTETFTGTHT--T");
+
+		board +=("\n");
+		//board +=("\n+--+--+--+--+--+--+--+--+--+--+\n");
+
+		for(Square[] square_list: squares) {
+			counter--;
+			//board +=("|");
+			board +=(counter);
+			board +=("T");
+			for(Square square: square_list) {
+				if(square.getPiece() != null) {
+					board +=(square.getPiece().toString() + "T");
+				} else {
+					board +=("-T");
+				}
+			}
+			board +=(counter);
+			board +=("");
+			board +=("\n");
+			//board +=("\n+--+--+--+--+--+--+--+--+--+--+\n");
+
+
+		}
+		List<String[]> list=new ArrayList<>();
+
+
+
+		int n =8;
+
+		String[][] arrayOfArrays = new String[n][9];
+		//System.out.println(board);
+		String [] BQ = board.split("\\n");
+		for (String x : BQ)
+		{
+
+			String[] re = x.split("T");
+
+			list.add(re);
+
+			for(int i=0;i<n;i++)
+			{
+				arrayOfArrays[i] = re;
+
+			}
+
+
+
+		}
+		//String[][] arr = new String[list.size()][];
+		arr = new String[list.size()][];
+		int i = 0;
+		for (String[] l: list) {
+			arr[i++] = l;
+		}
+
+
 	}
 }
